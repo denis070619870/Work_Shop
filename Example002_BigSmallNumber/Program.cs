66e6dbb3e5bdb659659c5b60380f39ee75e96e7c
Denis <denis070619870@rambler.ru>
2022-08-05 13:03:26 +0300

    Example002_BigSmallNumber

---
+++ Example002_BigSmallNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿// Найти из 2-х чисел какое наибольшее, а какое наименьшее
+
+int a = new Random().Next(1, 10);
+Console.WriteLine(a);
+int b = new Random().Next(1, 10);
+Console.WriteLine(b);
+
+if ( a > b)
+{
+    Console.WriteLine(a + " " + "Наибольшее число");
+    Console.WriteLine(b + " " + "Наименьшее число");
+}
+else
+{
+    Console.WriteLine(b + " " + "Наибольшее число");
+    Console.WriteLine(a + " " + "Наименьшее число");
+}
