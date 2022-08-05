be5437fabbac9a38020ca5f9795eade68bf0d47d
Denis <denis070619870@rambler.ru>
2022-08-05 13:28:49 +0300

    Example003_EvenNumbers

---
+++ Example003_EvenNumbers/Program.cs
@@ -0,0 +1,16 @@
+﻿// Показать четные числа от 1 до N
+
+Console.WriteLine("Введите число до которого будт показаны все четные числа: ");
+int userNumber = int.Parse(Console.ReadLine());
+int count = 1;
+
+Console.WriteLine(" ");
+while(count < userNumber)
+{
+    if(count % 2 ==0)
+    {
+        Console.WriteLine(count + " ");
+    }
+    count ++;
+}
+Console.WriteLine(" ");
