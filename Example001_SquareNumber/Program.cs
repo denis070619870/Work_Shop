bad60eab340425c359b76328d9855697bf712bdf
Denis <denis070619870@rambler.ru>
2022-08-05 12:32:19 +0300

    Example001_SquareNumber

---
+++ Example001_SquareNumber/Program.cs
@@ -0,0 +1,8 @@
+﻿// Посчитать квадрат числа
+
+Console.WriteLine("Введите число, а мы посчитаем квадрат:");
+int usernumber = Convert.ToInt16(Console.ReadLine());
+
+int result = usernumber * usernumber;
+
+Console.WriteLine("Квадрат числа" + " " + usernumber + " " + "равняется" + " " + result);
